bbd1ed61baecbbd56b4693efd6158fb3ec3495ed
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2019-08-22 16:20:55 -0400

    Add files via upload

--- regression/regression.py
+++ regression/regression.py
@@ -1,6 +1,26 @@
-import pandas as pd
-import quandl
+from statistics import mean
+import numpy as np
+import matplotlib.pyplot as plt 
+from matplotlib import style
 
-df = quandl.get("WIKI/GOOGL")
+style.use('fivethirtyeight')
 
-print(df.head())
+xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+
+def best_fit_slope_and_intercept(xs, ys):
+
+	m = (((mean(xs) * mean(ys)) - mean(xs * ys)) /
+		(mean(xs)**2 - mean(xs**2)))
+	b = mean(ys) - m*mean(xs)
+	
+	return m,b
+
+
+m,b = best_fit_slope_and_intercept(xs, ys)
+
+regression_line = [(m*x) + b for x in xs]
+
+plt.scatter(xs, ys)
+plt.plot(regression_line)
+plt.show()
