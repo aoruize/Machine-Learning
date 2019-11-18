46e46a7f3e683760ee5cb07bc1b09490f7b1f4bc
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2019-11-18 14:42:08 -0500

    pytorch intro udpate

--- Deep Learning/pytorch-intro.py
+++ Deep Learning/pytorch-intro.py
@@ -1,5 +1,13 @@
 from __future__ import print_function
 import torch
 
-x = torch.empty(5,3)
-print(x)
+x = torch.tensor([5.5, 3])
+x = x.new_ones(5, 3, dtype=torch.double)
+x = torch.randn_like(x, dtype=torch.float)
+y = torch.rand(5, 3)
+
+print(x[:, 1])
+print(y)
+
+
+
